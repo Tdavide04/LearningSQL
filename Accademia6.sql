a181a3bc848a74bda427f92e2c6e916d07dc8676
Tdavide04 <tdavide833@gmail.com>
2024-10-07 11:42:49 +0200

    update

--- Accademia6.sql
+++ Accademia6.sql
@@ -77,14 +77,71 @@ CREATE TABLE Assenza (
 -- Query 
 -- 1. Quanti sono gli strutturati di ogni fascia?
 
+select posizione, COUNT(*)
+from Persona
+GROUP BY posizione;
+
 -- 2. Quanti sono gli strutturati con stipendio ≥ 40000?
 
+select COUNT(*)
+from Persona
+where (stipendio >= 40000 );
+
 -- 3. Quanti sono i progetti già finiti che superano il budget di 50000?
+
+select COUNT(*) 
+from Progetto
+where fine < CURRENT_DATE AND budget > 50000;
+
 -- 4. Qual è la media, il massimo e il minimo delle ore delle attività relative al progetto ‘Pegasus’ ?
--- 5. Quali sono le medie, i massimi e i minimi delle ore giornaliere dedicate al progetto ‘Pegasus’ da ogni singolo docente?
--- 6. Qual è il numero totale di ore dedicate alla didattica da ogni docente?
+
+select 
+    AVG(oreDurata),
+    MAX(oreDurata),
+    MIN(oreDurata)
+from AttivitaProgetto, Progetto as p
+where progetto = p.id and p.nome = 'Pegasus';
+
+-- 5. Quali sono le medie, i massimi e i minimi delle ore giornaliere dedicate al progetto ‘Pegasus’ da ogni singolo docente? (non funziona)
+
+select 
+    per.nome
+    per.id
+    AVG(oreDurata),
+    MAX(oreDurata),
+    MIN(oreDurata)
+from AttivitaProgetto, Progetto as pro, Persona as per
+where progetto = pro.id and pro.nome = 'Pegasus';
+
+-- 6. Qual è il numero totale di ore dedicate alla didattica da ogni docente? (non funziona)
+
+select SUM(oreDurata), p.id, p.nome, p.cognome
+from AttivitaNonProgettuale, Persona as p
+where tipo = 'Didattica'
+group by persona;
+
 -- 7. Qual è la media, il massimo e il minimo degli stipendi dei ricercatori?
+
+select 
+    AVG(stipendio),
+    MAX(stipendio),
+    MIN(stipendio)
+from Persona
+where posizione = 'Ricercatore';
+
 -- 8. Quali sono le medie, i massimi e i minimi degli stipendi dei ricercatori, dei professori associati e dei professori ordinari?
+
+select 
+    posizione,
+    AVG(stipendio),
+    MAX(stipendio),
+    MIN(stipendio)
+from Persona
+where posizione in ('Ricercatore', 'Professore Associato', 'Professore Ordinario')
+group by posizione;
+
 -- 9. Quante ore ‘Ginevra Riva’ ha dedicato ad ogni progetto nel quale ha lavorato?
+
 -- 10. Qual è il nome dei progetti su cui lavorano più di due strutturati?
+
 -- 11. Quali sono i professori associati che hanno lavorato su più di un progetto?
