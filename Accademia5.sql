a3b3b3ed9d782ed05a3fb8cff96263a3d326ab11
Tdavide04 <tdavide833@gmail.com>
2024-10-10 11:38:30 +0200

    update

--- Accademia5.sql
+++ Accademia5.sql
@@ -1,18 +1,94 @@
--- 1. Quali sono il nome, la data di inizio e la data di fine dei WP del progetto di nome ‘Pegasus’ ?
+create type Strutturato as enum ('Ricercatore', 'Professore Associato', 'Professore Ordinario');
+create type LavoroProgetto as enum  ('Ricerca e Sviluppo', 'Dimostrazione', 'Management', 'Altro');
+create type LavoroNonProgettuale as enum ('Didattica', 'Ricerca', 'Missione', 'Incontro Dipartimentale', 'Incontro
+Accademico', 'Altro');
+create type CausaAssenza as enum ('Chiusura Universitaria', 'Maternita', 'Malattia');
+CREATE DOMAIN PostInteger AS INTEGER CHECK (VALUE >= 0);
+CREATE DOMAIN StringaM AS VARCHAR(100);
+CREATE DOMAIN NumeroOre AS INTEGER CHECK (VALUE >= 0 AND VALUE <= 8);
+CREATE DOMAIN Denaro AS REAL CHECK (VALUE >= 0);
 
-select WP.nome, WP.inizio, WP.fine
-from WP, Progetto
-where Progetto.id = WP.progetto and Progetto.nome = 'Pegasus';
+CREATE TABLE Persona (
+    id PostInteger NOT NULL, 
+    nome StringaM NOT NULL, 
+    cognome StringaM NOT NULL, 
+    posizione Strutturato NOT NULL, 
+    stipendio Denaro NOT NULL, 
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Progetto (
+    id PostInteger NOT NULL, 
+    nome StringaM NOT NULL, 
+    inizio DATE NOT NULL, 
+    fine DATE NOT NULL, 
+    budget Denaro NOT NULL, 
+    PRIMARY KEY (id), 
+    UNIQUE (nome),
+    CHECK (inizio < fine)
+);
+
+CREATE TABLE WP (
+    progetto PostInteger NOT NULL, 
+    id PostInteger NOT NULL, 
+    nome StringaM NOT NULL, 
+    inizio DATE NOT NULL, 
+    fine DATE NOT NULL, 
+    PRIMARY KEY (progetto, id), 
+    UNIQUE (nome), 
+    FOREIGN KEY (progetto) REFERENCES Progetto(id),
+    CHECK (inizio < fine)
+);
+
+CREATE TABLE AttivitaProgetto (
+    id PostInteger NOT NULL, 
+    persona PostInteger NOT NULL, 
+    progetto PostInteger NOT NULL, 
+    wp PostInteger NOT NULL, 
+    giorno DATE NOT NULL, 
+    tipo LavoroProgetto NOT NULL, 
+    oreDurata NumeroOre NOT NULL, 
+    PRIMARY KEY (progetto, id), 
+    FOREIGN KEY (persona) REFERENCES Persona(id), 
+    FOREIGN KEY (progetto, wp) REFERENCES WP(progetto, id)
+);
+
+CREATE TABLE AttivitaNonProgettuale (
+    id PostInteger NOT NULL, 
+    persona PostInteger NOT NULL, 
+    tipo LavoroNonProgettuale NOT NULL, 
+    giorno DATE NOT NULL, 
+    oreDurata NumeroOre NOT NULL, 
+    PRIMARY KEY (id), 
+    FOREIGN KEY (persona) REFERENCES Persona(id)
+);
+
+CREATE TABLE Assenza (
+    id PostInteger NOT NULL, 
+    persona PostInteger NOT NULL, 
+    tipo CausaAssenza NOT NULL, 
+    giorno DATE NOT NULL, 
+    PRIMARY KEY (id), 
+    UNIQUE (persona, giorno), 
+    FOREIGN KEY (persona) REFERENCES Persona(id)
+);
+
+
+-- Query:
+-- 1. Quali sono il nome, la data di inizio e la data di fine dei WP del progetto di nome 'Pegasus' ?
+
+select
+from 
+where
 
 -- 2. Quali sono il nome, il cognome e la posizione degli strutturati che hanno almeno una attività nel progetto ‘Pegasus’, ordinati per cognome decrescente?
-
-select distinct per.nome, per.cognome, per.posizione
-from Persona per, Progetto pro, AttivitaProgetto att
-where pro.nome = 'Pegasus' and att.persona = per.id and att.progetto = pro.id
-order by per.nome desc;
-
 -- 3. Quali sono il nome, il cognome e la posizione degli strutturati che hanno più di una attività nel progetto ‘Pegasus’ ?
-
-select per.nome, per.cognome, per.posizione
-from Persona per, Progetto pro, AttivitaProgetto att
-where pro.nome = 'Pegasus' and att.persona = per.id and att.progetto = pro.id and progetto > 1
+-- 4. Quali sono il nome, il cognome e la posizione dei Professori Ordinari che hanno fatto almeno una assenza per malattia?
+-- 5. Quali sono il nome, il cognome e la posizione dei Professori Ordinari che hanno fatto più di una assenza per malattia?
+-- 6. Quali sono il nome, il cognome e la posizione dei Ricercatori che hanno almeno un impegno per didattica?
+-- 7. Quali sono il nome, il cognome e la posizione dei Ricercatori che hanno più di un impegno per didattica?
+-- 8. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia attività progettuali che attività non progettuali?
+-- 9. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia attività progettuali che attività non progettuali? Si richiede anche di proiettare il giorno, il nome del progetto, il tipo di attività non progettuali e la durata in ore di entrambe le attività.
+-- 10. Quali sono il nome e il cognome degli strutturati che nello stesso giorno sono assenti e hanno attività progettuali?
+-- 11. Quali sono il nome e il cognome degli strutturati che nello stesso giorno sono assenti e hanno attività progettuali? Si richiede anche di proiettare il giorno, il nome del progetto, la causa di assenza e la durata in ore della attività progettuale.
+-- 12. Quali sono i WP che hanno lo stesso nome, ma appartengono a progetti diversi?
