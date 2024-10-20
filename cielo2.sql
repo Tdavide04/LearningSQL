e6abfc36024ec3e6c716863f55de3070b3d44a91
Tdavide04 <tdavide833@gmail.com>
2024-10-20 17:44:39 +0200

    cierlo 2 finito

--- cielo2.sql
+++ cielo2.sql
@@ -33,13 +33,57 @@ group by a.codice
 
 -- 6. Quante sono le nazioni (diverse) raggiungibili da ogni nazione tramite uno o più voli?
 
-select la.nazione, count(distinct la.nazione)
-from luogoaeroporto la
-where
+select lp.nazione, count(distinct la.nazione)
+from arrpart ar
+join luogoaeroporto lp on ar.partenza = lp.aeroporto
+join luogoaeroporto la on ar.arrivo = la.aeroporto
+where lp.nazione <> la.nazione
+group by lp.nazione;
 
 -- 7. Qual è la durata media dei voli che partono da ognuno degli aeroporti?
+
+select a.codice, a.nome, avg(v.durataminuti)
+from volo v
+join arrpart ar on v.codice = ar.codice and v.comp = ar.comp
+join aeroporto a on ar.partenza = a.codice
+group by a.codice
+
 -- 8. Qual è la durata complessiva dei voli operati da ognuna delle compagnie fondate a partire dal 1950?
+
+select ar.comp, sum(v.durataminuti) 
+from arrpart ar
+join volo v on ar.codice = v.codice 
+join compagnia c on c.nome = ar.comp
+where c.annofondaz >= 1950
+group by ar.comp
+
 -- 9. Quali sono gli aeroporti nei quali operano esattamente due compagnie?
+
+select a.codice, a.nome
+from aeroporto a
+join arrpart ar on (a.codice = ar.partenza or a.codice = ar.arrivo)
+group by a.codice
+having count(distinct ar.comp) = 2
+
 -- 10. Quali sono le città con almeno due aeroporti?
+
+select la.citta
+from luogoaeroporto la
+group by la.citta
+having count(distinct la.aeroporto) >= 2
+
 -- 11. Qual è il nome delle compagnie i cui voli hanno una durata media maggiore di 6 ore?
+
+select c.nome
+from compagnia c
+join arrpart ar on c.nome = ar.comp
+join volo v on v.codice = ar.codice
+group by c.nome
+having avg(v.durataminuti) > 360
+
 -- 12. Qual è il nome delle compagnie i cui voli hanno tutti una durata maggiore di 100 minuti?
+
+select v.comp
+from volo v
+group by v.comp
+having min(v.durataminuti) > 100
